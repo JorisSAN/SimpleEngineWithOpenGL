dc8f3ee3fb900d9960b7f9fb4b6e4c6c2456f582
JorisSAN <sancho.joris@gmail.com>
2020-10-19 12:39:27 +0200

    Update Cube.cpp

--- OpenGL/Cube.cpp
+++ OpenGL/Cube.cpp
@@ -7,7 +7,9 @@ Cube::Cube() :
 	velocite(Vector2(0.0f,0.0f)),
 	tourne(false),
 	x1(0),
-	xMouse(0)
+	xMouse(0),
+	y1(0),
+	yMouse(0)
 {
 	MeshComponent* mc = new MeshComponent(this);
 	mc->setMesh(Assets::getMesh("Mesh_Cube"));
@@ -18,16 +20,16 @@ void Cube::updateActor(float dt)
 	Quaternion q;
 	if (tourne)
 	{
-		SDL_GetMouseState(&xMouse, NULL);
-		rotation = Vector2((x1 - xMouse) / 20, 0.0f);
-		q = Quaternion::concatenate(q, Quaternion(Vector3::unitZ, rotation.x / 100));
-		setRotation(q);
+		SDL_GetMouseState(&xMouse, &yMouse);
+		rotation = Vector2((x1 - xMouse) / 20, (y1 - yMouse) / 20);
 	}
 	else
 	{
-		q = Quaternion::concatenate(q, Quaternion(Vector3::unitZ, rotation.x / 100));
-		setRotation(Quaternion::concatenate(getRotation(), q));
+		rotation *= 0.95;
 	}
+	q = Quaternion::concatenate(q, Quaternion(Vector3::unitZ, rotation.x / 100));
+	q = Quaternion::concatenate(q, Quaternion(Vector3::unitY, rotation.y / 100));
+	setRotation(q);
 }
 
 void Cube::actorInput(SDL_MouseButtonEvent& mouseEvent)
@@ -35,7 +37,7 @@ void Cube::actorInput(SDL_MouseButtonEvent& mouseEvent)
 	if (mouseEvent.button == SDL_BUTTON_LEFT && mouseEvent.type == SDL_MOUSEBUTTONDOWN)
 	{
 		tourne = true;
-		SDL_GetMouseState(&x1, NULL);
+		SDL_GetMouseState(&x1, &y1);
 	}
 	if (mouseEvent.button == SDL_BUTTON_LEFT && mouseEvent.type == SDL_MOUSEBUTTONUP)
 	{
